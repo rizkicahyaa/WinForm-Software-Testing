31125ba4b19e8ea2279403c34b5666adb57e2e67
Rizki Cahya <rizki.cahya03@gmail.com>
2025-10-27 22:20:23 +0700

    feat: setup CalculatorMachine

--- CalculatorConsoleTest/Program.cs
+++ CalculatorConsoleTest/Program.cs
@@ -4,12 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using CalculatorMachine;
+
 namespace CalculatorConsoleTest
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            Console.Title = "Demo Unit Testing Menggunakan App Console";
+            Calculator cal = new Calculator();
+
+            Console.WriteLine("hasil penjumlahan 2 + 3 = {0}", cal.Penambahan(2,3));
+            Console.WriteLine("hasil pengurangan 7 - 3 = {0}", cal.Pengurangan(7, 3));
+            Console.WriteLine("hasil perkalian 5 x 2 = {0}", cal.Perkalian(5, 2));
+            Console.WriteLine("hasil pembagian 6 + 2 = {0}", cal.Pembagian(6, 2));
+
+            Console.ReadKey();
         }
     }
 }
